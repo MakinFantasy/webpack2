c42367bf7e9dbe7ddcc2774fff35687cc9ebf1c8
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-10-26 00:31:07 +0300

    sent for review

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import {game, GameSavingData, writeGameSaving as saveGame, readGameSaving as loadGame} from './game'
+
+
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,25 @@
+import cayde from "./domain";
+
+
+class Game {
+    start() {
+        console.log('game started');
+    }
+}
+
+
+export class GameSavingData {
+
+}
+
+
+export function readGameSaving() {
+    
+}
+
+export function writeGameSaving() {
+
+}
+
+
+export default Game;
